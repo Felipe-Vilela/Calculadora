3e6eccdd834e7ada9d3c48004e58c986c6ecbb1e
Felipe Vilela <felipe.vilela01@outlook.com>
2025-08-23 15:47:47 -0300

    feat: inserindo numeros no visor

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/calculadora/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/calculadora/MainActivity.kt
@@ -13,6 +13,76 @@ class MainActivity : AppCompatActivity() {
         with(amb){
             setContentView(root)
 
+            zeroBt.setOnClickListener {
+                calculoTv.append("0")
+            }
+
+            umBt.setOnClickListener {
+                calculoTv.append("1")
+            }
+
+            doisBt.setOnClickListener {
+                calculoTv.append("2")
+            }
+
+            tresBt.setOnClickListener {
+                calculoTv.append("3")
+            }
+
+            quatroBt.setOnClickListener {
+                calculoTv.append("4")
+            }
+
+            cincoBt.setOnClickListener {
+                calculoTv.append("5")
+            }
+
+            seisBt.setOnClickListener {
+                calculoTv.append("6")
+            }
+
+            seteBt.setOnClickListener {
+                calculoTv.append("7")
+            }
+
+            oitoBt.setOnClickListener {
+                calculoTv.append("8")
+            }
+
+            noveBt.setOnClickListener {
+                calculoTv.append("9")
+            }
+
+            pontoBt.setOnClickListener {
+                calculoTv.append(".")
+            }
+
+
+            divisaoBt.setOnClickListener {
+                calculoTv.append("/")
+            }
+
+
+            multiplicacaoBt.setOnClickListener {
+                calculoTv.append("*")
+            }
+
+            somaBt.setOnClickListener {
+                calculoTv.append("+")
+            }
+
+            subtracaoBt.setOnClickListener {
+                calculoTv.append("-")
+            }
+
+            deleteBt.setOnClickListener {
+                calculoTv.text = calculoTv.text.dropLast(1)
+            }
+
+            acBt.setOnClickListener {
+                calculoTv.text = ""
+            }
+
         }
     }
 }
